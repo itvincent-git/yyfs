f68e92421b833b4f76e8af3e598b92a5740e521b
itvincent-git <itvincent@gmail.com>
2012-05-07 10:28:01 +0800

    编辑和列表

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
+app.register(".html", require('tenjin'));
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  //app.set('view engine', 'jade');
+  app.set('view options', {layout: false});
+  //app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  //app.use(express.logger());// ‰≥ˆhttp»’÷æ
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -27,12 +31,18 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
 // Routes
 console.log(routes);
+app.get('/edit', routes.edit);
 app.get('/', routes.index);
+app.all('/list', routes.list);
+
 app.post('/create', routes.create);
 app.get('/delKeys', routes.delKeys);
-app.get('/list', routes.list);
+app.get('/jade', routes.jade);
+app.get('/tenjin', routes.tengin);
+app.get('/ejs', routes.ejs);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,96 @@
-
-/*
- * GET home page.
- */
 // var ip = "172.16.102.46";
+var redis = require("redis");
+// redis.debug_mode = true;
 var ip = "127.0.0.1";
 exports.index = function(req, res){
-	// var redis = require("redis"), client = redis.createClient(6379, "172.16.102.46", null);
-	// client.set("foo:test:1000", "redis");
-	// client.get("foo:test:1000", function(err, re){
-		// console.log(re);
-		// res.render('index', { title: re + "123" })
-	// });
-	//var cid = req.param('cid');
-	
-	
-	res.redirect('/html/create.html');
+	res.render('index.html', {items : [{content : '123'}, {content : '<a href="#">adf</a>'}]});
+};
+
+exports.edit = function(req, res){
+	res.render('edit.html', {items : [{content : '123'}, {content : '<a href="#">adf</a>'}]});
 };
 
 exports.create = function(req, res){
-	var src_uid = req.param('src_uid');
-	var cid = req.param('cid');
-	var tar_uid = req.param('tar_uid');
-	var content = req.param('content');
 	console.dir(req.body);
-	var redis = require("redis"), client = redis.createClient(6379, ip, null);
+	var client = redis.createClient(6379, ip, null);
 	var id;
-	client.incr('yy:tzt:idgen', function(err, res){
+	var multi = client.multi();
+	client.incr('yy:tzt:idgen', function(err, reply){
 		if(err) console.error(err);
-		id = res;
-		console.log('id=' + id);
+		id = reply;
+		//console.log('id=' + id);
 		var tztKey = 'yy:tzt:' + id;
 		console.log('tztKey = ' + tztKey);
-		client.set(tztKey, JSON.stringify({src_uid : src_uid, cid : cid, tar_uid: tar_uid, content: content}), function(err1, res1){
-			if(err1) console.error(err1);
-			console.log('set =' + res1);
+		multi.set(tztKey, JSON.stringify({
+				id : id,
+				src_uid : req.param('src_uid'),
+				src_nick : req.param('src_nick'),
+				cid : req.param('cid'),
+				tar_uid: req.param('tar_uid'),
+				tar_nick : req.param('tar_nick'),
+				content: req.param('content')
+			}));
+		multi.rpush("yy:tzt:cid:" + req.param('cid'), id);
+		multi.exec(function(err, replies){
+			if(err){
+				console.error(err);
+			}else{
+				console.info(replies);
+				res.json({"result": "0"});
+			}			
+			client.quit();			
 		});
-		client.rpush("yy:tzt:cid:" + cid, id, function(err, res){
-			console.log('rpush = ' + res);
-		});
-	});
-	
-	res.json({"result": "0"});
-		
-	
+	});	
 };
 
 exports.list = function(req, res){
-	var cid = '12089499';
-	var redis = require("redis"), client = redis.createClient(6379, ip, null);
+	var cid = req.param('cid');
+	var client = redis.createClient(6379, ip, null);
 	client.lrange("yy:tzt:cid:" + cid, 0, -1, function(err, reply){
-		
-		//var list = JSON.parse(reply);
-		console.dir(reply);
-		res.send(reply);
-	});	
-	
-}
+		//console.dir(reply);
+		var multi = client.multi();
+		//遍历返回的纸条id
+		for(var i = 0; i < reply.length; i++){
+			var tztKey = 'yy:tzt:' + reply[i];
+			multi.get(tztKey);
+		}
+		multi.exec(function(err, replies){
+			client.quit();
+			if(err){
+				console.error(err);
+				return;
+			}
+			console.dir(replies);
+			var re = [];
+			for(var i = 0; i < replies.length; i++){
+				re.push(JSON.parse(replies[i]));
+			}			
+			res.send(re);
+		});		
+	});		
+};
 
 exports.delKeys =  function(req, res){
-	var redis = require("redis"), client = redis.createClient(6379, ip, null);
+	var client = redis.createClient(6379, ip, null);
 	client.del('yy:tzt:12089499', function(err, res){
 		console.log('del = ' + res);
 	});
 	res.send('ok');
-}
+};
+
+// exports.jade = function(req, res){
+	// res.render('index.jade');
+// };
+
+// exports.tengin = function(req, res){
+	// console.info('tengin');
+	// res.render('index.html', {name : '123'});
+// };
+
+exports.ejs = function(req, res){
+	// res.render('list', {
+	  // names: ['foo', 'bar', 'baz']
+	// });
+	res.send('ok');
+};
 

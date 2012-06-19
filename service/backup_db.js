95b990304ef40e358d1b64d7982fbaf0de2e5ea3
itvincent-git <itvincent@gmail.com>
2012-06-19 17:46:08 +0800

    change file name

--- service/backup_db.js
+++ service/backup_db.js
@@ -7,8 +7,28 @@ process.argv.forEach(function (val, index, array) {
     console.log(index + ': ' + val);
 });
 
+Date.prototype.format = function(format)
+{
+    var o = {
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    };
+    if(/(y+)/.test(format))
+        format=format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(var k in o)
+        if(new RegExp("("+ k +")").test(format))
+            format = format.replace(RegExp.$1,  RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
+};
+
 setInterval(function(){
-    child = exec(process.argv[2] + new Date().getTime(), {cwd:process.argv[3]},
+    var dateStr = new Date().format("yyyy-MM-dd-hh-mm-ss")
+    child = exec(process.argv[2] + dateStr,  {cwd:process.argv[3]},
         function (error, stdout, stderr) {
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
